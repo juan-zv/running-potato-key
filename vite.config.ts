9dad9935ea3b7944fedbba620f6cf987f0b82aa3
juan-zv <juanzuritavasquez@gmail.com>
2025-10-24 22:19:46 -0600

    synced packages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/running-potato-key/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
