606172086883e4761a19e1459a58ae4f7c8ac2ee
Efrain Gomez Fajardo <gom21023@byui.edu>
2025-10-25 14:16:07 -0600

    add types for User, Room, Image, and Task in schema

---
+++ src/components/db/schema.tsx
@@ -0,0 +1,39 @@
+type User = {
+    id: number
+    dob: Date
+    name: string
+    email: string
+    created_at: Date
+    bio: string
+    allergies: string
+    special_needs: string
+    pets: string
+    room_id: number
+    phone: string
+}
+
+type Room = {
+    id: number
+    created_at: Date
+    building: string
+    apt_num: string
+}
+
+type Image = {
+    id: number
+    url: string
+    title: string
+    category: string
+    room_id: number
+    created_by: number
+}
+
+type Task = {
+    id: number
+    desciription: string
+    assigned_to: number
+    completed: boolean
+    due_date: Date
+}
+
+export type { User, Room, Image, Task }
